a95530501a066a43f5748e085adee904ed6ebeb6
Vipul Yadav <vipulyadav0479@gmail.com>
2025-05-02 12:29:50 +0530

    name

--- src/pages/Termsconditions.tsx
+++ src/pages/Termsconditions.tsx
@@ -4,46 +4,46 @@ const Termsconditions = () => {
         <div className=" bg-gray-100 auto-h-screen">
         <div className="sm:mx-52 mx-4 mt-4 text-md font-sans">
             <p className="text-2xl font-semibold my-6 flex justify-center">Terms and Conditions</p>
-                <div>Through your use of our official website (https://www.optionandtrade.com/), and payment for a subscription to any of our research services, you acknowledge and agree to be bound by the terms and conditions listed below as well as all other terms and conditions, legal disclaimer, disclosures, policies, and user consent of Sameer Mekde as stated on our official website (https://www.optionandtrade.com/).</div>
+                <div>Through your use of our official website (https://www.optionandtrade.com/), and payment for a subscription to any of our research services, you acknowledge and agree to be bound by the terms and conditions listed below as well as all other terms and conditions, legal disclaimer, disclosures, policies, and user consent of arjun gehlot as stated on our official website (https://www.optionandtrade.com/).</div>
                 <br />
 
                 <div>These and any other terms and conditions, as well as the Legal Disclaimer, Disclosures, Policies, and User Consent, are subject to change at any moment. You agree to accept any new or changed terms and conditions as well as all other terms and conditions, our legal disclaimer, disclosures, policies, and user consent by continuing to use our website, and/or subscription to any of our research services. Please periodically check the links on our website for the Terms and Conditions, Legal Disclaimer, User Consent, Refund Policy, and Privacy Policy to be informed of any updates.</div>
 
                 <br />
-                <div>The term Sameer Mekde (https://www.optionandtrade.com/) is used throughout this entire document to refer to the website, its owners/proprietor, and the employees and associates of the owner. The words ‘You’ refer to the viewer/user/subscriber of Sameer Mekde’s website/mobile application/research services.</div>
+                <div>The term arjun gehlot (https://www.optionandtrade.com/) is used throughout this entire document to refer to the website, its owners/proprietor, and the employees and associates of the owner. The words ‘You’ refer to the viewer/user/subscriber of arjun gehlot’s website/mobile application/research services.</div>
                 </div>
 
                 <div className="mb-8 sm:mx-60 mx-8 text-base font-sans">
                 <ul className="list-disc lsit-inside">
                     <li>You hereby declare that the details provided by you at the time of subscription to our services are true and correct to the best of your knowledge and belief, and you undertake to inform us of any changes therein.</li>
                     <li>In case any of the above information is found to be false or misleading, you are aware that you may be held liable for it.
-                        You understand and accept that trading is a risky activity that involves the risk of loss of partial or complete capital and that there is no guarantee of profits or returns after subscribing to any of Sameer Mekde’s alerts services.</li>
+                        You understand and accept that trading is a risky activity that involves the risk of loss of partial or complete capital and that there is no guarantee of profits or returns after subscribing to any of arjun gehlot’s alerts services.</li>
                     <li>
-                        You understand and agree that Sameer Mekde and/or its owner / proprietor / managers / employees / associates do not provide any assurance or guarantee of accuracy or consistency of any of our research alerts / services.
+                        You understand and agree that arjun gehlot and/or its owner / proprietor / managers / employees / associates do not provide any assurance or guarantee of accuracy or consistency of any of our research alerts / services.
                     </li>
 
                     <li>Any accuracy level communicated to the user, whether in written or verbal form or in our advertisements / website, is merely for indicative purposes only.</li>
 
-                    <li>You understand and agree that the accuracy level / percentage of any of our research services / alerts may vary from time to time and there is no commitment from Sameer Mekde and/or its owner / proprietor / managers / employees / associates to deliver research services / alerts with a fixed accuracy level / percentage.</li>
+                    <li>You understand and agree that the accuracy level / percentage of any of our research services / alerts may vary from time to time and there is no commitment from arjun gehlot and/or its owner / proprietor / managers / employees / associates to deliver research services / alerts with a fixed accuracy level / percentage.</li>
 
-                    <li>You understand that for any/all trades taken by you, you will be held responsible for the outcome of the trade / investment either resulting in a gain or loss. You are willing to take complete responsibility for the outcome of all trades placed in your Demat account / trading account (Broker Account) during your tenure of the Sameer Mekde service subscription period.</li>
+                    <li>You understand that for any/all trades taken by you, you will be held responsible for the outcome of the trade / investment either resulting in a gain or loss. You are willing to take complete responsibility for the outcome of all trades placed in your Demat account / trading account (Broker Account) during your tenure of the arjun gehlot service subscription period.</li>
 
-                    <li>You understand that the service provided by Sameer Mekde is only a research service and is meant only for reference. You understand and accept that the service provided by Sameer Mekde is not investment advice and all decisions to trade/invest in any stock/instrument are solely yours and any loss/gain that may arise out of such trades/investments are solely your responsibility/liability.</li>
+                    <li>You understand that the service provided by arjun gehlot is only a research service and is meant only for reference. You understand and accept that the service provided by arjun gehlot is not investment advice and all decisions to trade/invest in any stock/instrument are solely yours and any loss/gain that may arise out of such trades/investments are solely your responsibility/liability.</li>
 
-                    <li>You agree to ALL terms and conditions, disclosures, disclaimers, policies, and user consent of Sameer Mekde, and you know very well about the risk associated with trading & investing in the stock market, and still, you wish to subscribe to the research services of Sameer Mekde. You have been informed completely about all the aspects of the services provided by Sameer Mekde, including the risks associated with trading and investing in the stock market.</li>
+                    <li>You agree to ALL terms and conditions, disclosures, disclaimers, policies, and user consent of arjun gehlot, and you know very well about the risk associated with trading & investing in the stock market, and still, you wish to subscribe to the research services of arjun gehlot. You have been informed completely about all the aspects of the services provided by arjun gehlot, including the risks associated with trading and investing in the stock market.</li>
 
-                    <li>You confirm and agree that you have taken the subscription to research services of Sameer Mekde after fully understanding the risks involved in trading and investing. You confirm and accept that you do not expect a fixed return or a guaranteed return from the research alerts service subscribed by you and you fully understand that you can / might lose your partial or entire capital in trading & investing by referring to research services / reports / alerts of Sameer Mekde.</li>
+                    <li>You confirm and agree that you have taken the subscription to research services of arjun gehlot after fully understanding the risks involved in trading and investing. You confirm and accept that you do not expect a fixed return or a guaranteed return from the research alerts service subscribed by you and you fully understand that you can / might lose your partial or entire capital in trading & investing by referring to research services / reports / alerts of arjun gehlot.</li>
 
-                    You have read and accepted all terms & conditions, legal disclaimer, refund policy, privacy policy, and user consent of Sameer Mekde mentioned on our official website (https://www.optionandtrade.com/).
+                    You have read and accepted all terms & conditions, legal disclaimer, refund policy, privacy policy, and user consent of arjun gehlot mentioned on our official website (https://www.optionandtrade.com/).
 
-                    You have read, understood, and accepted the risk mentioned in the disclaimer on the website/mobile app of Sameer Mekde (https://www.optionandtrade.com/).
-                    You understand and accept that trading in the stock market is subject to market risk and you understand and accept that you may lose some or all of your capital in stock market trading/investing activities, therefore, Sameer Mekde or its proprietor/owner cannot be held responsible for any losses in the stock market that you may incur and managing of trades is completely your responsibility.
+                    You have read, understood, and accepted the risk mentioned in the disclaimer on the website/mobile app of arjun gehlot (https://www.optionandtrade.com/).
+                    You understand and accept that trading in the stock market is subject to market risk and you understand and accept that you may lose some or all of your capital in stock market trading/investing activities, therefore, arjun gehlot or its proprietor/owner cannot be held responsible for any losses in the stock market that you may incur and managing of trades is completely your responsibility.
 
-                    You confirm that you have not been asked for your Demat trading account login ID & password by any of the employees/representatives of Sameer Mekde or anyone associated with Sameer Mekde, and your Demat/trading account login credentials are not disclosed/known to anyone except yourself.
+                    You confirm that you have not been asked for your Demat trading account login ID & password by any of the employees/representatives of arjun gehlot or anyone associated with arjun gehlot, and your Demat/trading account login credentials are not disclosed/known to anyone except yourself.
 
                     You declare that you will be trading / investing with your own personal capital and you will not be trading / investing with capital that is taken on a loan or capital belonging to someone other than yourself.
 
-                    You understand and accept that because you have been informed about all the risks in trading / investing and have taken the subscription package of Sameer Mekde’s trading research services/alerts, you have to bear the loss that may arise due to your trading activities. You also understand and accept that you cannot claim or complain against Sameer Mekde or its proprietor
+                    You understand and accept that because you have been informed about all the risks in trading / investing and have taken the subscription package of arjun gehlot’s trading research services/alerts, you have to bear the loss that may arise due to your trading activities. You also understand and accept that you cannot claim or complain against arjun gehlot or its proprietor
                 </ul>
         </div>
         </div>
